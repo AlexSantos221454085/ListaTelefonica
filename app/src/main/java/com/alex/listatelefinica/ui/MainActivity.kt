ad2dd44001467ff152f772281f64272bed1682dc
AlexSantos221454085 <alexsantos@edu.unifil.br>
2023-09-04 15:18:37 -0300

    commit logout

--- app/src/main/java/com/alex/listatelefinica/ui/MainActivity.kt
+++ app/src/main/java/com/alex/listatelefinica/ui/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.alex.listatelefinica.ui
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.alex.listatelefinica.R
+import com.alex.listatelefinica.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sharedPreferences = application.getSharedPreferences("login", Context.MODE_PRIVATE)
+
+        binding.btnLogout.setOnClickListener {
+            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+            editor.putString("username", "")
+            editor.apply()
+            finish()
+        }
     }
 }
