54a97e2ad9e9641f43032ae285ce45f9677e0019
AlexSantos221454085 <alexsantos@edu.unifil.br>
2023-09-06 15:07:19 -0300

    commit duas novas Activity e mostrando dados do banco na ListView

---
+++ app/src/main/java/com/alex/listatelefinica/ui/NewContactActivity.kt
@@ -0,0 +1,12 @@
+package com.alex.listatelefinica.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alex.listatelefinica.R
+
+class NewContactActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_contact)
+    }
+}
