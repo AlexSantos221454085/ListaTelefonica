83d8e7d3ec22ab92b5db3023fd3960305556793d
AlexSantos221454085 <alexsantos@edu.unifil.br>
2023-09-06 07:56:10 -0300

    commit craiando table contact

--- app/src/main/java/com/alex/listatelefinica/database/DBHelper.kt
+++ app/src/main/java/com/alex/listatelefinica/database/DBHelper.kt
@@ -4,13 +4,18 @@ import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.alex.listatelefinica.modelo.ContactModel
 import com.alex.listatelefinica.modelo.UserModel
+
 
 class DBHelper(context: Context) : SQLiteOpenHelper(context, "database.db", null, 1) {
 
     val sql = arrayOf(
         "CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, password TEXT)",
-        "INSERT INTO users (username, password) VALUES ('admin','password')"
+        "INSERT INTO users (username, password) VALUES ('admin','password')",
+        "CREATE TABLE contact (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, phone INT, imageId INT)",
+        "INSERT INTO contact (nome, email, phone, imageId) VALUES ('Maria', 'maria@mail.pt', 911222333,1)",
+        "INSERT INTO contact (nome, email, phone, imageId) VALUES ('João', 'joao@mail.pt', 912345678,2)",
     )
 
     override fun onCreate(db: SQLiteDatabase) {
@@ -20,9 +25,12 @@ class DBHelper(context: Context) : SQLiteOpenHelper(context, "database.db", null
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-
+        TODO("Not yet implmented")
     }
 
+    /*_________________________________________________________________________________________
+                                            CRUD USERS
+    _________________________________________________________________________________________*/
     fun insertUser(username: String, password: String): Long {
         val db = this.writableDatabase
         val contentValues = ContentValues()
@@ -43,7 +51,7 @@ class DBHelper(context: Context) : SQLiteOpenHelper(context, "database.db", null
         return res
     }
 
-    fun delete(id: Int): Int {
+    fun deleteUser(id: Int): Int {
         val db = this.writableDatabase
         val res = db.delete("users", "id=?", arrayOf(id.toString()))
         db.close()
@@ -58,6 +66,7 @@ class DBHelper(context: Context) : SQLiteOpenHelper(context, "database.db", null
         )
         var userModel = UserModel()
         if (c.count == 1) {
+            c.moveToFirst()
             val idIndex = c.getColumnIndex("id")
             val usernameIndex = c.getColumnIndex("username")
             val passwordIndex = c.getColumnIndex("password")
@@ -86,5 +95,96 @@ class DBHelper(context: Context) : SQLiteOpenHelper(context, "database.db", null
             db.close()
             return false
         }
+    }
+
+    /*_________________________________________________________________________________________
+                                        CRUD CONTACT
+_________________________________________________________________________________________*/
+    fun insertContact(name: String, email: String, phone: Int, imageId: Int): Long {
+        val db = this.writableDatabase
+        val contentValues = ContentValues()
+        contentValues.put("name", name)
+        contentValues.put("email", email)
+        contentValues.put("phone", phone)
+        contentValues.put("imageId", imageId)
+        val res = db.insert("contact", null, contentValues)
+        db.close()
+        return res
+    }
+
+    fun updateContact(id: Int, name: String, email: String, phone: Int, imageId: Int): Int {
+        val db = this.writableDatabase
+        val contentValues = ContentValues()
+        contentValues.put("name", name)
+        contentValues.put("email", email)
+        contentValues.put("phone", phone)
+        contentValues.put("imageId", imageId)
+        val res = db.update("contact", contentValues, "id=?", arrayOf(id.toString()))
+        db.close()
+        return res
+    }
+
+    fun deleteContact(id: Int): Int {
+        val db = this.writableDatabase
+        val res = db.delete("contact", "id=?", arrayOf(id.toString()))
+        db.close()
+        return res
+    }
+
+    fun getContact(id: Int): ContactModel {
+        val db = this.writableDatabase
+        val c = db.rawQuery(
+            "SELECT * FROM contact WHERE id=?",
+            arrayOf(id.toString())
+        )
+
+        var contactModel = ContactModel()
+
+        if (c.count == 1) {
+            c.moveToFirst()
+            val idIndex = c.getColumnIndex("id")
+            val nameIndex = c.getColumnIndex("name")
+            val emailIndex = c.getColumnIndex("email")
+            val phoneIndex = c.getColumnIndex("phone")
+            val imageIdIndex = c.getColumnIndex("imageId")
+
+            contactModel = ContactModel(
+                id = c.getInt(idIndex),
+                name = c.getString(nameIndex),
+                email = c.getString(emailIndex),
+                phone = c.getInt(phoneIndex),
+                imageId = c.getInt(imageIdIndex)
+            )
+        }
+        db.close()
+        return contactModel
+    }
+
+    fun getAllContact(id: Int): List<ContactModel> {
+        val db = this.writableDatabase
+        val c = db.rawQuery("SELECT * FROM contact", null)
+
+        var listaContactModel = ArrayList<ContactModel>()
+
+        if (c.count > 0) {
+            c.moveToFirst()
+            val idIndex = c.getColumnIndex("id")
+            val nameIndex = c.getColumnIndex("name")
+            val emailIndex = c.getColumnIndex("email")
+            val phoneIndex = c.getColumnIndex("phone")
+            val imageIdIndex = c.getColumnIndex("imageId")
+            do {
+                val contactModel = ContactModel(
+                    id = c.getInt(idIndex),
+                    name = c.getString(nameIndex),
+                    email = c.getString(emailIndex),
+                    phone = c.getInt(phoneIndex),
+                    imageId = c.getInt(imageIdIndex)
+                )
+                listaContactModel.add(contactModel)
+            } while (c.moveToNext())
+        }
+        db.close()
+        return listaContactModel
     }
 }
--- app/src/main/java/com/alex/listatelefinica/modelo/ContactModel.kt
+++ app/src/main/java/com/alex/listatelefinica/modelo/ContactModel.kt
@@ -1,10 +1,12 @@
 package com.alex.listatelefinica.modelo
 
-class ContactModel {
-    val id: Int = 0
-    val name: String = ""
-    val address: String = ""
-    val email: String = ""
-    val phone: String = ""
-    val imageID: Int = 0
-}
+data class ContactModel(
+    val id: Int =0,
+    val name: String = "",
+    val address: String = "",
+    val email: String = "",
+    val phone: Int = 0,
+    val imageId: Int = 0,
+)
+
+
